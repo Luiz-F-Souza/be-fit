796db021297bca1103a31eb3f60678894e84029b
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-03-10 18:48:08 -0300

    home page finalizada

--- src/scenes/home/Home.tsx
+++ src/scenes/home/Home.tsx
@@ -5,6 +5,7 @@ import HomePageGraphic from '../../assets/HomePageGraphic.png'
 import SponsorRedBull from '../../assets/SponsorRedBull.png'
 import SponsorForbes from '../../assets/SponsorForbes.png'
 import SponsorFortune from '../../assets/SponsorFortune.png'
+import { motion } from 'framer-motion'
 
 
 
@@ -14,7 +15,18 @@ function Home(){
     <article id="home" >
       
       <div className='flex flex-col sm:flex-row items-center w-full  justify-center h-full pb-12 px-12 '>
-        <section>
+        
+        <motion.section 
+          className=' sm:w-2/4' 
+          initial="hidden" 
+          whileInView='visible' 
+          viewport={{ once: true, amount:0.5 }} 
+          transition={{ duration: 0.75 }} 
+          variants={{ 
+            hidden: { opacity: 0, x: -50 }, 
+            visible: {opacity: 1, x: 0}}
+            }
+        >
 
           <header className='relative mb-8'>
             <img src={HomePageBackImage} alt='EvoGym backLogo'  className='absolute -z-10 -top-20 left-2 hidden md:block'/>
@@ -23,12 +35,19 @@ function Home(){
 
           <div className='px-1'>
             <p className='text-sm  pb-4'>
-              Unrivaled Gym. Unparalleled Training Fitness Classes.
-              World Class Studios to get the Body Shapes That you Dream of..
-              Get Your Dream Body Now.
+              Conquiste o corpo dos seus sonhos na Unrivaled Gym. Treinamento incomparável, aulas de fitness e estúdios de classe mundial. Comece agora!
             </p>
 
-            <section className=''>
+            <motion.section className=''
+              initial="hidden" 
+              whileInView='visible' 
+              viewport={{ once: true, amount:0.5 }} 
+              transition={{ duration: 1.25 }} 
+              variants={{ 
+                hidden: { opacity: 0, x: -50 }, 
+                visible: {opacity: 1, x: 0}}
+                }
+            >
                 <Btn
                   label='Faça parte'
                   onClick={()=> console.log('saiba mais btn')}
@@ -54,21 +73,21 @@ function Home(){
                     textColor: ''
                   }}
                 />
-            </section>
+            </motion.section>
           </div>
 
-        </section>
+        </motion.section>
 
-        <section className=''>
+        <section className='flex justify-center'>
 
           <img src={HomePageGraphic} alt="mulher com roupa de academia e bem treinada."  className='w-5/6'/>
         </section>
       </div>
 
-      <section className='gap-6 py-6 px-12 hidden sm:flex bg-primary-100'>
-        <img src={SponsorRedBull} alt='logo patrocinador: RedBull'/>
-        <img src={SponsorForbes} alt='logo patrocinador: Forbes'/>
-        <img src={SponsorFortune} alt='logo patrocinador: Fortune'/>
+      <section className='gap-8 py-6 px-12 hidden sm:flex bg-primary-100'>
+        <img src={SponsorRedBull} alt='logo patrocinador: RedBull' width={80} height={80}/>
+        <img src={SponsorForbes} alt='logo patrocinador: Forbes' width={80} height={80}/>
+        <img src={SponsorFortune} alt='logo patrocinador: Fortune' width={80} height={80}/>
       </section>
     </article>
   )
