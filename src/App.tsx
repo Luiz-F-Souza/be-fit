879840f9899939b8387aa34265190d0be8cb69f7
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-03-12 16:19:37 -0300

    ourclasses scene sendo criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/navbar/Navbar"
 import { Benefits } from "./scenes/benefits/Benefits"
 import { Home } from "./scenes/home/Home"
+import { OurClasses } from "./scenes/our-classes/OurClasses"
 
 
 
@@ -8,18 +9,21 @@ function App() {
 
 
   return (
-    <div className="app min-h-screen relative min-h-[300vh]">
+    <div className="app min-h-screen relative">
       <header className="pb-24 md:pb-40">
         <Navbar />
       </header>
 
-      <main className="mb-4">
+      <main className="mb-4 overflow-x-hidden">
 
         {/* Article */}
         <Home />
 
         {/* Article */}
         <Benefits />
+
+        {/* Article */}
+        <OurClasses />
       </main>
 
 
---
+++ src/scenes/our-classes/OurClasses.tsx
@@ -0,0 +1,17 @@
+
+
+function OurClasses(){
+
+  return(
+    <article className="bg-primary-100 px-12 py-24">
+
+      <section>
+        <h3 className="font-bold text-3xl mb-4">NOSSAS TURMAS</h3>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, accusamus possimus porro animi ut eveniet asperiores earum soluta consequuntur maxime velit qui culpa cum quasi natus doloremque accusantium tenetur nemo!</p>
+      </section>
+    </article>  
+  )
+}
+
+export { OurClasses }
